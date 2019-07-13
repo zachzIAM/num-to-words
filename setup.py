f796b0617023799e5f44eab3d5d0864bfe49c02b
Zach Zankov <zach.zankov@investecmail.com>
2019-07-13 22:17:12 +0100

    to make installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='num-to-words',
+    version='1.0.0',
+    packages=['num2words'],
+    url='https://github.com/zachzIAM/num-to-words',
+    license='MIT',
+    author='Zach Zankov',
+    author_email='zach.zankov@investecmail.com',
+    description='Convert a number to English numeral'
+)
